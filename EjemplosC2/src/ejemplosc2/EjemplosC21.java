cb4e538a44673538fe60e18380a78cda7943d52f
BrandonVS <vegasotobrandon@yahoo.com>
2019-04-11 10:43:25 -0500

    Clase2

---
+++ EjemplosC2/src/ejemplosc2/EjemplosC21.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosc2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salas
+ */
+public class EjemplosC21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String nombre;
+        String apellido;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor ingrese sus nombres: ");
+        nombre = entrada.nextLine(); 
+        System.out.println("Por favor ingrese sus apellidos: ");
+        apellido = entrada.nextLine();
+        System.out.println("Su nombre es: " + nombre + "\n\n\tSu apellido es: "
+               + apellido);
+        
+    }
+
+}
